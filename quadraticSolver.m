5d6ac89cfe11f8c6a882d1e2dbac16d9dc9e1629
Vishnu <vishnudas.padinhare-purakkal@rntbci-nissan.com>
2026-07-29 14:03:06 +0530

    Updated quadraticsolver script for find error

--- quadraticSolver.m
+++ quadraticSolver.m
@@ -10,7 +10,7 @@ if a == 0
     else
         x = -c/b;
         fprintf('Linear root = %.2f\n', x);
-    end
+
 else
     D = b^2 - 4*a*c;
 
@@ -22,5 +22,5 @@ else
         fprintf('Root 2 = %.2f\n', x2);
     else
         disp('Complex roots')
-    end
+
 end
